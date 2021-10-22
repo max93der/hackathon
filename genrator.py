6ec2f812b8e1e32e561e4aebef4c16c9eb2bdabb
Alex <alexandreandries23@gmail.com>
2021-10-23 01:45:30 +0200

    generateur + premier set de data

--- genrator.py
+++ genrator.py
@@ -1,4 +1,5 @@
 import csv
+import random as rd
 from parking import *
 
 
@@ -14,33 +15,18 @@ from parking import *
 header = ["name","xCoord","yCoord","maxCap","morningOcc","noonOcc","eveningOcc"]
 
 data = [
-    ['Parking_A', 28748, 'AL', 'ALB'],
-    ['Parking_B', 2381741, 'DZ', 'DZA'],
-    ['Parking_C', 199, 'AS', 'ASM'],
-    ['Parking_D', 468, 'AD', 'AND'],
-    ['Parking_B28', 1246700, 'AO', 'AGO'],
-    ['Parking_Trifacultaire', 1246700, 'AO', 'AGO'],
-    ['Parking_B52', 1246700, 'AO', 'AGO'],
-    ['Parking_B37', 1246700, 'AO', 'AGO']
+    ['Parking_A', 790, 175, 200, rd.randint(0, 200), rd.randint(0, 200), rd.randint(0, 200)],
+    ['Parking_B', 900, 165, 200, rd.randint(0, 200), rd.randint(0, 200), rd.randint(0, 200)],
+    ['Parking_C', 960, 145, 200, rd.randint(0, 200), rd.randint(0, 200), rd.randint(0, 200)],
+    ['Parking_D', 1045, 90, 50, rd.randint(0, 50), rd.randint(0, 50), rd.randint(0, 50)],
+    ['Parking_B28', 420, 220, 80, rd.randint(0, 80), rd.randint(0, 80), rd.randint(0, 80)],
+    ['Parking_Trifacultaire', 1125, 185, 120, rd.randint(0, 120), rd.randint(0, 120), rd.randint(0, 120)],
+    ['Parking_B52', 300, 160, 70, rd.randint(0, 70), rd.randint(0, 70), rd.randint(0, 70)],
+    ['Parking_B37', 400, 155, 30, rd.randint(0, 30), rd.randint(0, 30), rd.randint(0, 30)]
 ]
 
 with open('parking_gen.csv', 'w', encoding='UTF8', newline='') as file:
-     
-    writer =  csv.writer(file) 
 
+    writer =  csv.writer(file)
     writer.writerow(header)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    writer.writerows(data)
