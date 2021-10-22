24e9d9ed9898c33631adc63e9b5881c6e5de36da
Abdelilahkh <Abdelilah.Khaliphi@student.uliege.be>
2021-10-22 23:10:41 +0200

    pas important

--- parking.py
+++ parking.py
@@ -3,8 +3,8 @@ from pygame import draw
 class parking():
 
     def __init__(self, location_tp,total_spaces, available_spaces, electrical_spaces, is_covered, price_hour, ):
-        
-        
+
+
         self.total_spaces      = total_spaces
         self.location_tp       = location_tp
         self.available_spaces  = available_spaces
