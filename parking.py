2f5750bddcfe75f05a74d5b65f99973244d0e6d2
Marzouk Ouro-Gomma <marzouk.ouro-gomma@student.uliege.be>
2021-10-22 23:34:55 +0200

    affluance to rgb function

--- parking.py
+++ parking.py
@@ -12,3 +12,18 @@ class parking():
         self.affluance         =  affluance
 
 
+    def afflluance_to_rgb(parking):
+        
+        if parking.affluance < 33:
+            return (0, 255, 0)
+        elif parking.affluance < 33 and parking.affluance > 66:
+            return (255, 128, 0)
+        elif parking.affluance < 66:
+            return (255, 0, 0)
+
+    
+
+
+    
+    
+    
