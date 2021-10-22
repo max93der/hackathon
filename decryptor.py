66459a5375717cbfa9309063b5b20393de538685
Marzouk Ouro-Gomma <marzouk.ouro-gomma@student.uliege.be>
2021-10-23 01:33:44 +0200

    decryptor done

--- decryptor.py
+++ decryptor.py
@@ -4,13 +4,15 @@ from parking import *
 
 
 def decrypt(filename):
-
+    parking_list = []
     with open(filename) as file:
         reader  = csv.reader(file, delimiter=',')
         line_count = 0
 
         for row in reader:
             if line_count != 0:
-                
-                
+                parking_list.append(parking(row[0], row[1], row[2], row[3], row[4], row[5], row[6]) ) 
+                line_count += 1
+
+    return parking_list
         
