6d999e95736366c4f3265b63013bacdd437b71be
Marzouk Ouro-Gomma <marzouk.ouro-gomma@student.uliege.be>
2021-10-22 21:25:41 +0200

    class parking next

--- parking.py
+++ parking.py
@@ -3,5 +3,5 @@ class parking:
     def __init__(self, location_tp, available_spaces, electrical_spaces, is_covered, price_hour):
 
         self.location_tp = location_tp
-        self.available
-        self.
+        self.available_spaces = available_spaces
+        self.electrical_spaces = electrical_spaces
