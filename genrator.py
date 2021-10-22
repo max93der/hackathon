ed42f6c7ef5099ebe44f7f9c7bfe40091721e041
Marzouk Ouro-Gomma <marzouk.ouro-gomma@student.uliege.be>
2021-10-23 01:15:01 +0200

    whatever

--- genrator.py
+++ genrator.py
@@ -5,4 +5,21 @@ from parking import *
 header = ["name","xLocation","yLocation", "affluance", "total_capacity", "current_capacity","electiral_spaces",]
 with open('parking_gen.csv', 'w', encoding='UTF8', newline='') as file:
      
-    writer =  csv.writer(file)  
+    writer =  csv.writer(file) 
+
+    writer.writerow(header)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
