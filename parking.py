401ca9f4b6538c9e626969bd46f60ddc62a9739e
Marzouk Ouro-Gomma <marzouk.ouro-gomma@student.uliege.be>
2021-10-22 23:19:03 +0200

    parking.py update

--- parking.py
+++ parking.py
@@ -1,13 +1,11 @@
 import pygame
-from pygame import draw
+
 class parking():
 
-    def __init__(self, location_tp,total_spaces, available_spaces, electrical_spaces, is_covered, price_hour, ):
+    def __init__(self, location_tp, affluance,  electrical_spaces, is_covered, price_hour, ):
 
 
-        self.total_spaces      = total_spaces
         self.location_tp       = location_tp
-        self.available_spaces  = available_spaces
         self.electrical_spaces = electrical_spaces
         self.is_covered        = is_covered
         self.price_hour        = price_hour
