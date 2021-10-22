9c7f486cc02f767fd0deb6234f3c22ba7ccb252e
Abdelilahkh <Abdelilah.Khaliphi@student.uliege.be>
2021-10-22 21:29:45 +0200

    .

--- parking.py
+++ parking.py
@@ -2,6 +2,8 @@ class parking:
 
     def __init__(self, location_tp, available_spaces, electrical_spaces, is_covered, price_hour):
 
-        self.location_tp = location_tp
-        self.available_spaces = available_spaces
+        self.location_tp       = location_tp
+        self.available_spaces  = available_spaces
         self.electrical_spaces = electrical_spaces
+        self.is_covered        = is_covered
+        self.price_hour        = price_hour
