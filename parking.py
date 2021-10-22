09cb6936f4e447c6487a743355d26d97984df403
Marzouk Ouro-Gomma <marzouk.ouro-gomma@student.uliege.be>
2021-10-22 21:20:27 +0200

    classe parking

--- parking.py
+++ parking.py
@@ -1,3 +1,7 @@
 class parking:
 
-    
+    def __init__(self, location_tp, available_spaces, electrical_spaces, is_covered, price_hour):
+
+        self.location_tp = location_tp
+        self.available
+        self.
